01ab73ec3fde7f208e4d08bc59fd2c86d7a13106
John Lecegues <68977770+lecegues@users.noreply.github.com>
2024-08-25 06:09:27 +0000

    Updates for robustness, tweaked YDL options for better quality, added 2 more commands
    - YDL_OPTIONS: updated for better music quality
    - Bug fixes and updates for robustness
    - 2 commands added: /shuffle, /nowplaying

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -1,9 +1,10 @@
 import discord
+import random
 from discord.ext import commands
 from yt_dlp import YoutubeDL
 
-QUEUE_SONG_LIMIT = 5  # Max number of songs to display in /queue
 
+QUEUE_SONG_LIMIT = 5  # Max number of songs to display in /queue
 
 class YoutubeCog(commands.Cog):
 
@@ -15,10 +16,11 @@ class YoutubeCog(commands.Cog):
         self.is_paused = False
 
         # [ ( {"source": url, "title": title}, voice_channel_obj )]
+        # where [0] is the NEXT song
         self.music_queue = []
+        self.curr_song = None 
 
         # YoutubeDL options
-        # self.YDL_OPTIONS = {"format": "bestaudio", "noplaylist": "True"}
         self.YDL_OPTIONS = {
             "format":
             "m4a/bestaudio/best",
@@ -27,7 +29,14 @@ class YoutubeCog(commands.Cog):
             "postprocessors": [{
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "m4a",
-            }]
+                "preferredquality": "0",  # 0 is best quality
+            }],
+            "postprocessor_args": [
+                "-ar",
+                "48000",
+                "-ac",
+                "2"  # ensure stereo output
+            ]
         }
 
         # FFMPEG options to handle reconnection
@@ -36,32 +45,6 @@ class YoutubeCog(commands.Cog):
             "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
             "options": "-vn -loglevel debug"
         }
-        '''def search_yt(self, query: str) -> dict | bool:
-        """
-        Search YouTube with given query
-
-        Returns: dict: {"source": url, "title" title}
-        """
-        with YoutubeDL(self.YDL_OPTIONS) as ydl:
-            try:
-                # perform yt search and extract info from first result without downloading the video
-                info = ydl.extract_info("ytsearch:%s" % query, download=False)
-                if not info:
-                    raise ValueError(f"No results found for {query}")
-                info = info['entries'][0]
-                print(f"INFO: {info}")
-                # make sure format is an audio stream
-                formats = info.get('formats', [])
-                for f in formats:
-                    if f.get(
-                            'acodec'
-                    ) != 'none':  # acodec should not be 'none' for audio streams
-                        return {"source": f["url"], "title": info["title"]}
-                print("No audio stream found")
-                return False  # No suitable audio format found
-            except Exception as e:
-                print(e)
-                return False'''
 
     def search_yt(self, query: str) -> dict | bool:
         """
@@ -82,18 +65,19 @@ class YoutubeCog(commands.Cog):
                 print(e)
                 return False
 
-    def play_next(self):
+    async def play_next(self) -> None:
         """Plays the next song in the music queue if available; otherwise stops music"""
         if len(self.music_queue) > 0:
             self.is_playing = True
 
             # get url of next song in queue and remove it from queue
             m_url = self.music_queue[0][0]["source"]
-            self.music_queue.pop(0)
+            self.curr_song = self.music_queue.pop(0)
 
             # Play audio with options, and after, call this function again to play next song
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS),
                          after=lambda e: self.play_next())
+
         else:
             self.is_playing = False
 
@@ -114,37 +98,41 @@ class YoutubeCog(commands.Cog):
                 else:
                     await self.vc.move_to(self.music_queue[0][1])
 
-            self.music_queue.pop(0)
+            self.curr_song = self.music_queue.pop(0)
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS),
                          after=lambda e: self.play_next())
         else:
             self.is_playing = False
 
     @discord.slash_command(name="play",
-                           description="Play the selected song from YouTube")
+                           description="Play the selected song from YouTube. Can be a URL or a search query")
     async def play(self, ctx, query: str):
-        """Allow users to search for songs and play it in their voice chat"""
+        """
+        Allow users to search for songs and play it in their voice chat
 
-        voice_channel = ctx.author.voice.channel
+        Post-Conditions:
+        - If not connected: Prompt them to connect
+        - If already playing a song: Add song to queue
+        """
+
+        voice_channel = ctx.author.voice
         if voice_channel is None:
             await ctx.respond("Connect to a voice channel!")
-        elif self.is_paused:
-            self.vc.resume()
         else:
+            voice_channel = voice_channel.channel
             # Search for the song on YT using the provided query
             await ctx.defer()
             song = self.search_yt(query)
-            if song == False:  # Check if the search failed
+            if not song:  # Check if the search failed
                 await ctx.respond(
                     "Could not download the song. Incorrect format, try a different keyword"
                 )
             else:
-                await ctx.respond("Song added to the queue")
-
                 # add song + voice channel to queue
                 self.music_queue.append([song, voice_channel])
+                await ctx.respond(f"{self.music_queue[-1][0]["title"]} added to the queue")
 
-                if self.is_playing == False:
+                if not self.is_playing:
                     await self.play_music(ctx)
 
     @discord.slash_command(name="pause", description="Pause the current song")
@@ -156,7 +144,7 @@ class YoutubeCog(commands.Cog):
             self.vc.pause()
             await ctx.respond("Music paused.")
         elif self.is_paused:
-            self.vc.resume()
+            await ctx.respond("Music is already paused.")
 
     @discord.slash_command(name="resume",
                            description="Resume the current song")
@@ -167,13 +155,21 @@ class YoutubeCog(commands.Cog):
             self.is_paused = False
             self.vc.resume()
             await ctx.respond("Music resumed.")
+        elif self.is_playing:
+            await ctx.respond("Music is already playing.")
 
     @discord.slash_command(name="skip", description="Skip the current song")
     async def skip(self, ctx):
-        if self.vc != None and self.vc:
-            self.vc.stop()
-            await self.play_music(ctx)
-
+        if self.vc and self.vc.is_playing(): 
+            self.vc.stop() 
+            await self.play_next()
+            if self.curr_song: 
+                await ctx.respond(f"Now playing {self.curr_song[0]['title']}")
+            else: 
+                await ctx.respond("No more songs in queue")
+        else: 
+            await ctx.respond("No music playing to skip.")
+    
     # @TODO display in a code block
     @discord.slash_command(name="queue", description="Show the current queue")
     async def queue(self, ctx):
@@ -193,7 +189,7 @@ class YoutubeCog(commands.Cog):
     @discord.slash_command(name="clear", description="Clear the current queue")
     async def clear(self, ctx):
         """Clear the queue and stop the currently playing song"""
-        if self.vc != None and self.is_playing:
+        if self.vc and self.is_playing:
             self.vc.stop()
         self.music_queue = []
         await ctx.respond(f"Music queue cleared")
@@ -205,13 +201,31 @@ class YoutubeCog(commands.Cog):
         self.is_playing = False
         self.is_paused = False
         await self.vc.disconnect()
+        await ctx.respond("Disconnected from the voice channel")
 
-    @discord.slash_command(name="youtube",
-                           description="Ask me if I have YouTube features!")
-    async def youtube(self, ctx):
-        print("Slash Command: YouTube activated.")
-        await ctx.respond("Not yet! Haha 😂")
+    @discord.slash_command(name="nowplaying", description="Show the current song name")
+    async def now_playing(self, ctx): 
+        """Show the title of the current song playing"""
+        if self.vc and self.is_playing and self.curr_song: 
+            await ctx.respond(f"Now playing {self.curr_song[0]['title']}")
+        else: 
+            await ctx.respond("No music playing")
 
+    @discord.slash_command(name="shuffle", description="Shuffle the current queue")
+    async def shuffle(self, ctx): 
+        """Shuffle the current queue"""""
+        if self.music_queue: 
+            random.shuffle(self.music_queue) 
+            await ctx.respond("Music queue shuffled")
+        else: 
+            await ctx.respond("No music in the queue")
+            
+    # /lyrics: use an API to find the lyrics for the song if it exists
+    # /remove: remove a song from the queue
+    # /repeat? Clone the current song to the next spot in the queue
+    # /playlist: adds a playlist to the queue
+    # /seek or /jump??-- can either go forward/backwards a certain time OR jump to a certain time 
+    # /songinfo-- provide detailed information about the currently playing song? 
 
 def setup(bot):
     bot.add_cog(YoutubeCog(bot))
